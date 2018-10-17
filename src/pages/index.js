c2031ef88c2f0d20d173beea8a4edc796c4145bd
Cjarit <cjarit@gmail.com>
2018-10-17 11:39:29 +0700

    test git

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default class Index extends React.Component {
         <h1>Hi people</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
+        <p>test git</p>
         <Link to="/page-2/">Go to page 2</Link>
       </div>
     )
